b20e2c8bbcb444d5a2e1e98312678ca186ef0f5f
elhoss1 <hosam123138@gmail.com>
2026-02-22 19:02:14 +0300

    Question 02

--- G-Net-34-OOP02/BankAccount.cs
+++ G-Net-34-OOP02/BankAccount.cs
@@ -40,6 +40,24 @@ namespace G_Net_34_OOP02
         //Developing the code in the future will be very difficult because it will require modification everywhere you use it.
         #endregion
         #endregion
+        #region Question 02
+        //Field is a variable that is declared directly in a class or struct. It can be public, private, protected, internal, or protected internal. It can be of any type, including primitive types, reference types, and value types. Fields are used to store data that is associated with an object or class.
+        //Property is a member that provides a flexible mechanism to read, write, or compute the value of a private field. Properties can be used as if they are public data members, but they are actually special methods called accessors. The get accessor returns the property value, and the set accessor assigns a new value to the property. Properties can also include validation logic in the set accessor to ensure that the value being assigned is valid.
+        // yes Can a property contain logic
+        // public string name;  // Field
+        // public string Name { get; set; } // Auto-implemented property
+        //private int width;
+        //private int height;
+
+        //// خاصية لقراءة المساحة (وهي قيمة محسوبة)
+        //public int Area
+        //{
+        //    get
+        //    {
+        //        return width * height; // المساحة هي الطول × العرض
+        //    }
+        //}
+        #endregion
         #endregion
     }
 }
